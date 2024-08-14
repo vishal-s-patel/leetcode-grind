ec17435b355a593aec53afea7c31e56c9787e735
sriram727788 <90505347+sriram727788@users.noreply.github.com>
2024-08-14 20:38:37 +0530

    Update pattern.js
    pattern added

--- pattern.js
+++ pattern.js
@@ -43,3 +43,102 @@ for(let i =1;i<=row; i++){
 }
 console.log(' array based pattern');
 console.log(pattern);
+
+// -------------------------------------------------- 3rd pattern -------------------------
+/*
+1
+ 1 2
+ 1 2 3
+ 1 2 3 4
+ 1 2 3 4 5
+*/
+
+let row = 5;
+let pattern = [];
+for(let i =1;i<=row; i++){
+    let pattern_sub = [];
+    let sub_str = '';
+    for(let j =1;j<=i; j++){
+        sub_str = sub_str+' '+ j;
+        pattern_sub.push(j);
+    }
+    console.log(sub_str);
+    pattern.push(pattern_sub);
+}
+console.log(' array based pattern');
+console.log(pattern);
+
+// -------------------------------------------------- 4th Pattern ----------------------------------------------
+
+/*
+ 1
+ 2 2
+ 3 3 3
+ 4 4 4 4
+ 5 5 5 5 5
+*/
+let row = 5;
+let pattern = [];
+for(let i =1;i<=row; i++){
+    let pattern_sub = [];
+    let sub_str = '';
+    for(let j =1;j<=i; j++){
+        sub_str = sub_str+' '+ i;
+        pattern_sub.push(i);
+    }
+    console.log(sub_str);
+    pattern.push(pattern_sub);
+}
+console.log(' array based pattern');
+console.log(pattern);
+
+// --------------------------------------------------- 5th pattern ------------------------------------------------------------------
+/*
+ *  *  *  *  * 
+ *  *  *  * 
+ *  *  * 
+ *  * 
+ *
+*/
+
+let row = 5;
+let column = 5;
+let pattern = [];
+for(let i =1;i<=row; i++){
+    let pattern_sub = [];
+    let sub_str = '';
+    for(let j =0;j<=column -i; j++){
+        sub_str = sub_str+' * ';
+        pattern_sub.push(' * ');
+    }
+    console.log(sub_str);
+    pattern.push(pattern_sub);
+}
+console.log(' array based pattern');
+console.log(pattern);
+
+
+// ----------------------------------------------------- 6th pattern ----------------------------------------------------------------------------------
+/*
+ 1 2 3 4 5
+ 1 2 3 4
+ 1 2 3
+ 1 2
+ 1
+*/
+
+let row = 5;
+let column = 5;
+let pattern = [];
+for(let i =0;i<=row; i++){
+    let pattern_sub = [];
+    let sub_str = '';
+    for(let j =1;j<=column -i; j++){
+        sub_str = sub_str+' '+j;
+        pattern_sub.push( j );
+    }
+    console.log(sub_str);
+    pattern.push(pattern_sub);
+}
+console.log(' array based pattern');
+console.log(pattern);
