c74c9a6d6c90e47d17bd2b52520260a434929ec5
sriram727788 <90505347+sriram727788@users.noreply.github.com>
2024-08-14 20:19:30 +0530

    Update pattern.js
    pattern 2 written

--- pattern.js
+++ pattern.js
@@ -20,3 +20,26 @@ for(let i =0;i<row; i++){
 }
 console.log(' array based pattern');
 console.log(pattern);
+
+// -------------------------------------    2nd pattern -----------------------------------------
+
+/*
+ * 
+ *  * 
+ *  *  * 
+ *  *  *  * 
+*/
+let row = 4;
+let pattern = [];
+for(let i =1;i<=row; i++){
+    let pattern_sub = [];
+    let sub_str = '';
+    for(let j =0;j<i; j++){
+        sub_str = sub_str+' * ';
+        pattern_sub.push(' * ');
+    }
+    console.log(sub_str);
+    pattern.push(pattern_sub);
+}
+console.log(' array based pattern');
+console.log(pattern);
