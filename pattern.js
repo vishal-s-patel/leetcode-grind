983d1b21f9fdd82e5c483319c1b866ab1e1e75a1
sriram727788 <90505347+sriram727788@users.noreply.github.com>
2024-08-17 09:36:29 +0530

    Update pattern.js
    Added more pattern

--- pattern.js
+++ pattern.js
@@ -281,3 +281,235 @@ for(let i=0; i<row-1; i++){
     console.log(sub_str)
 }
 console.log(str_arr);
+
+// ------------------------------------------------------------ 11th pattern --------------------------------------------------------
+/*
+1      1
+12    21
+123  321
+12344321
+*/
+
+let row = 4;
+for(let i=1; i<=row; i++){
+    let str ='';
+    for(let j =1; j<=i;j++){
+        str = str+''+j;
+    }
+    
+    for(let k=1; k<=(row-i)*2; k++){
+         str = str+' ';
+    }
+    
+    for(let j=i; j>=1;j--){
+        str = str+''+j;
+    }
+    console.log(str);
+}
+// -------------------------------------------------------- 12th pattern ----------------------------------------------------------------------
+
+/*
+ 1
+ 2 3
+ 4 5 6
+ 7 8 9 10
+*/
+
+let row = 4;
+let count =1;
+for(let i=1; i<=row; i++){
+    let str ='';
+    for(let j =1; j<=i;j++){
+        str = str+' '+count;
+        count++;
+    }
+    console.log(str);
+}
+
+// -------------------------------------------------------- 13th pattern ----------------------------------------------------------------------
+/*
+ A
+ A B
+ A B C
+ A B C D
+ A B C D E
+*/
+let row = 5;
+for(let i=1; i<=row; i++){
+    let str ='';
+    for(let j =1; j<=i;j++){
+        str = str+' '+String.fromCharCode(64+j);
+    }
+    console.log(str);
+}
+
+// -------------------------------------------------------- 14th pattern ----------------------------------------------------------------------
+/*
+ A B C D E
+ A B C D
+ A B C
+ A B
+ A
+*/
+let row = 5;
+for(let i=0; i<row; i++){
+    let str ='';
+    for(let j =1; j<=row-i;j++){
+        str = str+' '+String.fromCharCode(64+j);
+    }
+    console.log(str);
+}
+
+
+// -------------------------------------------------------- 15th pattern ----------------------------------------------------------------------
+/*
+ A
+ B B
+ C C C
+ D D D D
+ E E E E E
+*/
+let row = 5;
+for(let i=1; i<=row; i++){
+    let str ='';
+    for(let j =1; j<=i;j++){
+        str = str+' '+String.fromCharCode(64+i);
+    }
+    console.log(str);
+}
+
+// -------------------------------------------------------- 16th pattern ----------------------------------------------------------------------
+/*
+    A
+   ABA
+  ABCBA
+ ABCDCBA
+ABCDEDCBA
+*/
+
+let row = 5;
+for(let i=1; i<=row; i++){
+    let str ='';
+    for(let j =1; j<=row-i;j++){
+        str = str+' ';
+    }
+    for(let j =1; j<=i;j++){
+        str = str+''+String.fromCharCode(64+j);
+    }
+      for(let j =1; j<=i-1;j++){
+        str = str+''+String.fromCharCode(64+i-j);
+    }
+    console.log(str);
+}
+
+// ----------------------------------------------------------- 17th pattern --------------------------------------------------------------------
+/*
+**********
+****  ****
+***    ***
+**      **
+*        *
+*        *
+**      **
+***    ***
+****  ****
+**********
+*/
+
+let row = 5;
+for(let i=0; i<row; i++){
+    let str ='';
+    for(let j =0; j<row-i;j++){
+        str = str+'*';
+    }
+     for(let j =0; j<i*2;j++){
+        str = str+' ';
+    }
+     for(let j =0; j<row-i;j++){
+        str = str+'*';
+    }
+    console.log(str);
+}
+for(let i=1; i<=row; i++){
+    let str ='';
+    for(let j =0; j<i;j++){
+        str = str+'*';
+    }
+    for(let j =0; j<(row-i)*2;j++){
+        str = str+' ';
+    }
+     for(let j =0; j<i;j++){
+        str = str+'*';
+    }
+    console.log(str);
+}
+
+// ------------------------------------------------------------- 18th pattern -----------------------------------------------
+/*
+
+*        *
+**      **
+***    ***
+****  ****
+**********
+****  ****
+***    ***
+**      **
+*        *
+
+*/
+let row = 5;
+for(let i=1; i<=row; i++){
+    let str ='';
+    for(let j =0; j<i;j++){
+        str = str+'*';
+    }
+    for(let j =0; j<(row-i)*2;j++){
+        str = str+' ';
+    }
+     for(let j =0; j<i;j++){
+        str = str+'*';
+    }
+    console.log(str);
+}
+for(let i=1; i<row; i++){
+    let str ='';
+    for(let j =0; j<row-i;j++){
+        str = str+'*';
+    }
+     for(let j =0; j<i*2;j++){
+        str = str+' ';
+    }
+     for(let j =0; j<row-i;j++){
+        str = str+'*';
+    }
+    console.log(str);
+}
+
+// ---------------------------------------------- 19th pattern ----------------------------------------------------
+/*
+*****
+*   *
+*   *
+*   *
+*****
+*/
+let row = 5;
+for(let i=1; i<=row; i++){
+    let str ='';
+    if(i==1 || i==row){
+        for(let j =0; j<row;j++){
+            str = str+'*';
+        }
+    }else{
+        for(let j =1; j<=row;j++){
+             if(j==1 || j==row){
+                 str = str+'*';
+             }else{
+                 str = str+' ';
+             }
+            
+        }
+    }
+    console.log(str);
+}
